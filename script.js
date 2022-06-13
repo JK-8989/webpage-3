83cdb672292a67f6c60186ca8ae732d3303d9916
JK-8989 <jiyoungkim365@gmail.com>
2022-06-13 14:17:31 -0700

    basic markup

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector('.grid');
+const input = document.getElementById('input');
+const submitBtn = document.getElementById('submit');
+
+
+let macyInstance = Macy({
+  container: grid
+});
+
+const key = 'kZgLlrhYYwDB80VmUxDdydI7lGb_5PSazfbo31hrUKI';
+
+const API_URL = 'https://api.unsplash.com'
